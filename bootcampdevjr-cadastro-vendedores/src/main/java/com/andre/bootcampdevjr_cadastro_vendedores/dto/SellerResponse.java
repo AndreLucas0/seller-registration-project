81a4837bdae417bc5ce03c0b54e79fea2f60eb08
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-10-28 22:33:43 -0300

    feat: get by id method #6

---
+++ bootcampdevjr-cadastro-vendedores/src/main/java/com/andre/bootcampdevjr_cadastro_vendedores/dto/SellerResponse.java
@@ -0,0 +1,5 @@
+package com.andre.bootcampdevjr_cadastro_vendedores.dto;
+
+public class SellerResponse {
+    
+}
--- bootcampdevjr-cadastro-vendedores/src/main/java/com/andre/bootcampdevjr_cadastro_vendedores/service/SellerService.java
+++ bootcampdevjr-cadastro-vendedores/src/main/java/com/andre/bootcampdevjr_cadastro_vendedores/service/SellerService.java
@@ -3,7 +3,10 @@ package com.andre.bootcampdevjr_cadastro_vendedores.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.andre.bootcampdevjr_cadastro_vendedores.model.Seller;
 import com.andre.bootcampdevjr_cadastro_vendedores.repository.SellerRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SellerService {
@@ -11,6 +14,11 @@ public class SellerService {
     @Autowired
     private SellerRepository sellerRepository;
 
-    
+    public SellerResponse getById(long id) {
+        Seller seller = sellerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Seller not found."));
+
+        
+    }
 
 }
